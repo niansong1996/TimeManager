e36b4eb042ed8806ab1eee434d833207c9ad0c03
Anthony Ni <niansong1996@gmail.com>
2015-11-14 22:04:11 +0800

    commit

--- src/com/company/test.java
+++ src/com/company/test.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class test {
 
 	public static void main(String[] args) {
-		
+		char a = '倪';
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		if(s.charAt(0)==a) System.out.println("true");
 	}
 
 }
